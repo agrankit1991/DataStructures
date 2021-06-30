57b69f24e6604979d66d4520c48a4cc88df0bfc5
Ankit Agrawal <ankitagrawal1991@gmail.com>
2021-06-30 19:00:50 +0530

    Recursion: CumulativeSum

--- src/com/agrankit/datastructures/recursion/FibonacciSeries.java
+++ src/com/agrankit/datastructures/recursion/FibonacciSeries.java
@@ -2,7 +2,9 @@ package com.agrankit.datastructures.recursion;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        printSeries(0, 1, 10);
+        int n = 7;
+        printSeries(0, 1, n);
+        System.out.println(getNth(0, 1, n));
     }
 
     private static void printSeries(int f0, int f1, int n) {
@@ -12,5 +14,12 @@ public class FibonacciSeries {
 
         System.out.println(f0);
         printSeries(f1, f0+f1, n-1);
+    }
+
+    private static int getNth(int f0, int f1, int n) {
+        if (n == 1) {
+            return f0;
+        }
+        return getNth(f1, f0+f1, n-1);
     }
 }
